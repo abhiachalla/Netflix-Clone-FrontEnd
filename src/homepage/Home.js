224c42e8dd557f286d2c86f17658f7354729e478
abhiachalla <70327235+abhiachalla@users.noreply.github.com>
2021-12-21 23:50:52 -0500

    Add files via upload
    Please run using npx create-react-app

---
+++ src/homepage/Home.js
@@ -0,0 +1,22 @@
+
+import { AcUnit } from "@material-ui/icons"
+import Navbar from "../components/navbar/Navbar.js"
+import Featured from "../components/featured/featured.jsx"
+
+import "./Home.css"
+
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <Navbar/>
+           
+            <Featured/ >
+
+
+        </div>
+    )
+}
+
+export default Home
+
